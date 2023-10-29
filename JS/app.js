23aa9f5b6708537381fce2570a7db21100e616f1
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-10-29 18:33:32 +0100

    passage des lettres ok

--- JS/app.js
+++ JS/app.js
@@ -72,9 +72,11 @@ const recordShow = document.querySelector(".record");
 const word = document.querySelector(".word");
 let score = 0;
 let record;
-let chosenWord;
 let chosenLetter;
+let chosenWord=initialWords[getRandomNumber(initialWords.length)].toUpperCase();
+let wordInArray = chosenWord.split("");
 
+// gestion record
 if (!localStorage.getItem("record")) {
   localStorage.setItem("record", 0);
   record = 0;
@@ -84,38 +86,46 @@ if (!localStorage.getItem("record")) {
   recordShow.textContent = record;
 }
 
-// console.log(letters);
 
 function checkLetter(chosenLetter) {
-  console.log(chosenLetter);
+  wordInArray.forEach((letter, index) => {
+    if(letter===chosenLetter){
+      letters_word[index].textContent = letter
+    }
+  
+  })
 }
 
+// choix lettre sur écran
 letters.forEach((letter) => {
   letter.addEventListener("click", () => {
     chosenLetter = letter.outerText;
     checkLetter(chosenLetter);
   });
 });
-
+// choix lettre sur clavier
 document.addEventListener("keydown", (event) => {
   if (event.key.length === 1 && event.key.match(/[a-zA-Z]/)) {
-    chosenLetter = event.key;
+    chosenLetter = event.key.toUpperCase();
     checkLetter(chosenLetter);
   }
 });
-
-function updateScore() {}
-
+// nb aléatoire pour sélection mot dans liste
 function getRandomNumber(max) {
   return Math.floor(Math.random() * max);
 }
-
+// selection d'un mot de la liste et affichage
 function showWord() {
-    let wordInArray = initialWords[getRandomNumber(initialWords.length)].split("");
-  console.log(wordInArray);
-    wordInArray.forEach(letter => {
-      word.innerHTML+='<span class="letter_word"><h2>'+letter+'</h2></span>'
-    });
+ 
+  wordInArray.forEach(letter => {
+    let h2Element = document.createElement('h2');
+    h2Element.className = 'letter_word';
+    h2Element.textContent = '_'
+    word.appendChild(h2Element); 
+  });
 }
-
 showWord();
+const letters_word = document.querySelectorAll('.letter_word')
+console.log(letters_word);
+console.log(chosenWord[0]);
+function updateScore() {}
