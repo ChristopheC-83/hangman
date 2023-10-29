8c35d664b53a5333862cb909dce1436284ab8d3b
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-10-29 11:28:28 +0100

    darkmode checkbox fonctionnel, reste style à faire

--- JS/darkMode.js
+++ JS/darkMode.js
@@ -1,15 +1,24 @@
 const toggleDarkMode = document.querySelector(".checkbox");
-const body =document.querySelector('body')
+const body = document.querySelector("body");
 
-toggleDarkMode.addEventListener("change", function(){
-    const toggleChecked = this.checked
-    
+if (!localStorage.getItem("darkModeMemory")) {
+  localStorage.setItem("darkModeMemory", false);
+  body.classList.remove("dark");
+} else {
+  if (localStorage.getItem("darkModeMemory") === true) {
+    body.classList.add("dark");
+  } else {
+    body.classList.remove("dark");
+  }
+}
 
-    console.log("toggleChecked");
-    if (toggleChecked) {
-        body.classList.add('dark')
-    } else {
-        body.classList.remove('dark')
-    }
-})
-
+toggleDarkMode.addEventListener("change", function () {
+  let toggleChecked = this.checked;
+  if (toggleChecked) {
+    localStorage.setItem("darkModeMemory", false);
+    body.classList.remove("dark");
+  } else {
+    localStorage.setItem("darkModeMemory", true);
+    body.classList.add("dark");
+  }
+});
